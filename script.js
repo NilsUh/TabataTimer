f7d0c09757c4a34f2fa3c5acaf6193120142ac1f
Nils Uhlemann <nils.uhlemann@web.de>
2024-02-18 17:20:05 +0100

    added CTabataTimer class

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-var nrSets;
-var secSetsBreak;
-var nrReps;
-var secReps;
-var secRepsBreak;
+class CTabataTimer {
+  constructor(nrSets, secSetsBreak, nrReps, secReps, secRepsBreak) {
+    this.nrSets = nrSets;
+    this.set = 0;
+    this.secSetsBreak = secSetsBreak;
+    this.nrReps = nrReps;
+    this.rep = 0;
+    this.secReps = secReps;
+    this.secRepsBreak = secRepsBreak;
+    this.timeElapsed = 0;
+    this.timeRemaining = nrSets * nrReps * secReps + (nrSets - 1) * secSetsBreak + (nrReps - 1) * secRepsBreak;
+    this.timerId = null;
 
-var secTotal;
-var secRemaining;
-var secElapsed;
+    this.setDisplays();
+    this.startCountdown();
+  }
 
-var set = 1;
-var rep = 1;
+  startCountdown() {
+    this.timerId = setInterval(() => {
+      if (this.timeRemaining > 0) {
+        this.timeRemaining--;
+        this.timeElapsed++;
+        this.setDisplays();
+      } else {
+        clearInterval(this.timerId);
+      }
+    }, 1000);
+  }
+
+  setDisplays() {
+    const [minutesRemaining, secondsRemaining] = this.calcMinSec(this.timeRemaining);
+    const [minutesElapsed, secondsElapsed] = this.calcMinSec(this.timeElapsed);
+    const progress = this.timeElapsed / (this.timeElapsed + this.timeRemaining);
+
+    document.getElementById("set-display").textContent = this.set.toString() + "/" + this.nrSets.toString();
+    document.getElementById("rep-display").textContent = this.rep.toString() + "/" + this.nrReps.toString();
+    document.getElementById("time-remaining-display").textContent = minutesRemaining.toString() + ":" + secondsRemaining.toString();
+    document.getElementById("time-elapsed-display").textContent = minutesElapsed.toString() + ":" + secondsElapsed.toString();
+    document.getElementById("progress-display").textContent = progress.toString() + "%";
+  }
+
+  calcMinSec(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return [mins, secs];
+  }
+}
 
 function run() {
   /* read inputs */
-  nrSets = document.getElementById("nrSetsInput").value;
-  secSetsBreak = document.getElementById("breakLengthSetsInput").value;
-  nrReps = document.getElementById("nrRepsPerSetInput").value;
-  secReps = document.getElementById("secsPerRepInput").value;
-  secRepsBreak = document.getElementById("breakLengthRepsInput").value;
+  var nrSets = document.getElementById("nrSetsInput").value;
+  var secSetsBreak = document.getElementById("breakLengthSetsInput").value;
+  var nrReps = document.getElementById("nrRepsPerSetInput").value;
+  var secReps = document.getElementById("secsPerRepInput").value;
+  var secRepsBreak = document.getElementById("breakLengthRepsInput").value;
 
   /* disable inputs */
   document.getElementById("nrSetsInput").disabled = true;
@@ -26,16 +61,8 @@ function run() {
   document.getElementById("secsPerRepInput").disabled = true;
   document.getElementById("breakLengthRepsInput").disabled = true;
 
-  /* calculate training time */
-  secTotal = nrSets * nrReps * secReps + (nrSets - 1) * secSetsBreak + (nrReps - 1) * secRepsBreak;
-  secRemaining = secTotal;
-  secElapsed = 0;
-  progress = secElapsed / secTotal;
+  tabataTimer = new CTabataTimer(nrSets, secSetsBreak, nrReps, secReps, secRepsBreak);
+};
 
-  /* initial update of displays */
-  document.getElementById("set-display").textContent = set.toString() + "/" + nrSets.toString();
-  document.getElementById("rep-display").textContent = rep.toString() + "/" + nrReps.toString();
-  document.getElementById("time-remaining-display").textContent = secRemaining.toString();
-  document.getElementById("time-elapsed-display").textContent = secElapsed.toString();
-  document.getElementById("progress-display").textContent = progress.toString() + "%";
-}
+
+document.getElementById("run-botton").addEventListener("click", run);
