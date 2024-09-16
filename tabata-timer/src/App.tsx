efa6012e63b50b371497ae16f164adb747391042
Nils Uhlemann <nils.uhlemann@web.de>
2024-09-16 19:52:15 +0200

    adding Alert component to showcase passing children as argument

--- tabata-timer/src/App.tsx
+++ tabata-timer/src/App.tsx
@@ -1,31 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = [
-    "Set 1/3 - Round 1/2",
-    "Set 2/3 - Round 1/2",
-    "Set 3/3 - Round 1/2",
-    "Set 1/3 - Round 2/2",
-    "Set 2/3 - Round 2/2",
-    "Set 3/3 - Round 2/2",
-  ];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Sets and Rounds"
-        onSelectItem={handleSelectItem}
-      />
-      <ListGroup
-        items={["TestA", "TestB"]}
-        heading="Test List"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
